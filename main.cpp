1944d1b910113b9c1eec07c8f89252941a4a8c90
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-04-03 21:59:26 +0100

    add array printer example

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <print>
 
-struct foo {
+struct foo_t {
   int a, b;
 };
-struct bar {
-  struct foo x, y;
+struct bar_t {
+  foo_t x, y;
+};
+struct arr_t {
+  int* v;
+  int size;
 };
 
 int main(int argc, char** argv) {
   std::println("hello, world");
 
-  foo f = {.a = 30, .b = 25};
-  foo f2 = {.a = 12, .b = 123};
-  bar b = {.x = f, .y = f2};
+  foo_t f = {.a = 30, .b = 25};
+  foo_t f2 = {.a = 12, .b = 123};
+  bar_t b = {.x = f, .y = f2};
 
-  foo fs[20] = {};
+  foo_t fs[20] = {};
   fs[0] = f;
   fs[1] = f2;
+
+  arr_t arr;
+  arr.v = new int[10];
+  arr.size = 10;
+
+  for (int i = 0; i < 10; i++) {
+    arr.v[i] = i;
+  }
 
   return 0;
 }
--- my_gdb_pretty_printers/custom_printers.py
+++ my_gdb_pretty_printers/custom_printers.py
@@ -21,8 +21,31 @@ class barPrinter(gdb.ValuePrinter):
         return "x={" + str(self.__val["x"]) + "} y={" + str(self.__val["y"]) + "}"
 
 
+class arrPrinter(gdb.ValuePrinter):
+
+    def __init__(self, val):
+        self.__val = val
+
+    def to_string(self):
+        size = int(self.__val["size"])
+        return f"arr_t(size={size})"
+
+    def children(self):
+        data_ptr = self.__val["v"]
+        if data_ptr == 0:
+            return
+
+        size = int(self.__val["size"])
+        for i in range(size):
+            yield f"[{i}]", (data_ptr + i).dereference()
+
+    def display_hint(self):
+        return "array"
+
+
 def build_pretty_printers():
     pp = gdb.printing.RegexpCollectionPrettyPrinter("custom_printers")
-    pp.add_printer("foo", "^foo$", fooPrinter)
-    pp.add_printer("bar", "^bar$", barPrinter)
+    pp.add_printer("foo_t", "^foo_t$", fooPrinter)
+    pp.add_printer("bar_t", "^bar_t$", barPrinter)
+    pp.add_printer("arr_t", "^arr_t$", arrPrinter)
     return pp
